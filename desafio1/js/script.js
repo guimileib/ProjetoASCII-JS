a697499a8c0ce8001441661ef87838986ecff93b
guimileib <guilhermemileib@gmail.com>
2025-02-27 12:20:43 -0300

    feat: Implementing video background

--- desafio1/js/script.js
+++ desafio1/js/script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const botao = document.getElementById("btnCriar");
     botao.addEventListener("click", criarMorcego);
+    
 
     if(botao.addEventListener("click", criarMorcego)){
         console.log("Botão clicado");
@@ -12,21 +13,30 @@ document.addEventListener("DOMContentLoaded", function() {
 function criarMorcego() {
     console.log("btn clicado");
 
-    const morcego = document.createElement("img");
-    morcego.src = "/img/morcego_unic.png";
-    morcego.style.position = "absolute";
-    morcego.style.width = "300px";
-    morcego.style.left = Math.random() * window.innerWidth + "px";
-    morcego.style.top = Math.random() * window.innerHeight + "px";
-    morcego.style.transition = "transform 2s linear";
+    let videoContainer = document.getElementById("video-container");
+    let video = document.getElementById("video");
 
-    // Colocando fundo ao apertar o botão
-    //const fundo = style.createElement("iframe");
-    //fundo.src = 
-    document.body.appendChild(morcego);
+    videoContainer.style.display = "block";
+    video.src ="https://www.youtube.com/embed/ddyKLEb_G38?autoplay=1&mute=1&loop=1&playlist=ddyKLEb_G38";
+    video.autoplay = true;
+    const botao = document.getElementById("btnCriar");
+    botao.style.display = "none";
+    
+    
+    for (let i = 0; i < 10; i++) {
+        const morcego = document.createElement("img");
+        morcego.src = "/img/morcego_unic.png";
+        morcego.style.position = "absolute";
+        morcego.style.width = "300px";
+        morcego.style.left = Math.random() * window.innerWidth + "px";
+        morcego.style.top = Math.random() * window.innerHeight + "px";
+        morcego.style.transition = "transform 2s linear";
 
-    setTimeout(() => {
-        morcego.style.transform = "translateY(-100vh) rotateY(45deg) rotateX(45deg) scale(1.5)";
-        setTimeout(() => morcego.remove(), 2000);
-    }, 100);
+        document.body.appendChild(morcego);
+
+        setTimeout(() => {
+            morcego.style.transform = "translateY(-100vh) rotateY(45deg) rotateX(45deg) scale(1.5)";
+            setTimeout(() => morcego.remove(), 2000);
+        }, 100);
+    }
 }
