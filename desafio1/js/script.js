ab38a1b5b8344c4d7b2d9c999691bcfabafee81a
guimileib <guilhermemileib@gmail.com>
2025-02-27 15:19:30 -0300

    feat: Implementing video in 2 parts

--- desafio1/js/script.js
+++ desafio1/js/script.js
@@ -1,13 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Js carregado");
 
-    const botao = document.getElementById("btnCriar");
-    botao.addEventListener("click", criarMorcego);
-    
+    let videoInicial = document.getElementById("videoInicial");
+    let videoContainer = document.getElementById("videoContainer");
+    let botao = document.getElementById("btnCriar");
 
-    if(botao.addEventListener("click", criarMorcego)){
-        console.log("Botão clicado");
-    }
+    videoContainer.style.display = "block";
+    videoInicial.play();
+
+    document.body.addEventListener("click", function iniciarVideo() {
+        videoInicial.play().catch(error => console.error("Erro ao tentar iniciar o vídeo:", error));
+        document.body.removeEventListener("click", iniciarVideo); 
+    });
+
+    videoInicial.addEventListener("ended", function() {
+        console.log("Video finalizado, botao aparecendo");
+        botao.style.display = "block";
+    });
+
+   
+    botao.addEventListener("click", criarMorcego);
+
 });
 
 function criarMorcego() {
@@ -16,29 +29,37 @@ function criarMorcego() {
     const somMorcego = new Audio("/audio/bat_fly.mp3");
     somMorcego.play();
 
+    let videoInicial = document.getElementById("videoInicial");
     let videoContainer = document.getElementById("videoContainer");
-    let videoBtn = document.getElementById("videoBtn");
+    let videoBtn = document.getElementById("videoBtt");
+    let botao = document.getElementById("btnCriar");
+
+    videoInicial.style.display = "none";
     videoContainer.style.display = "block";
+    videoBtn.style.display = "block";
+    videoBtn.style.visibility = "visible";
+    videoBtn.style.opacity = "1";
     videoBtn.play();
 
-    //const botao = document.getElementById("btnCriar");
-    //botao.style.display = "none";
-    
-    
+    botao.style.display = "none";
+
     for (let i = 0; i < 20; i++) {
-        const morcego = document.createElement("img");
-        morcego.src = "/img/morcego_unic.png";
-        morcego.style.position = "absolute";
-        morcego.style.width = "300px";
-        morcego.style.left = Math.random() * window.innerWidth + "px";
-        morcego.style.top = Math.random() * window.innerHeight + "px";
-        morcego.style.transition = "transform 5.5s linear";
-
-        document.body.appendChild(morcego);
-
         setTimeout(() => {
-            morcego.style.transform = "translateY(-100vh) rotateY(45deg) rotateX(45deg) scale(1.5)";
-            setTimeout(() => morcego.remove(), 5000);
-        }, 200);
-    }
+            const morcego = document.createElement("img");
+            morcego.src = "/desafio1/img/morcego_unic.png";
+            morcego.style.position = "absolute";
+            morcego.style.width = "300px";
+            morcego.style.left = Math.random() * window.innerWidth + "px";
+            morcego.style.top = Math.random() * window.innerHeight + "px";
+            morcego.style.transition = "transform 5.5s linear";
+    
+            document.body.appendChild(morcego);
+    
+            setTimeout(() => {
+                morcego.style.transform = "translateY(-100vh) rotateY(45deg) rotateX(45deg) scale(1.5)";
+                setTimeout(() => morcego.remove(), 5000);
+            }, 200);
+        }, i * 500); 
+    }   
+    
 }
