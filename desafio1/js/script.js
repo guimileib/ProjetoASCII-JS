2f96db5dd6399c04dc271d4d8aa13fb075437de3
guimileib <guilhermemileib@gmail.com>
2025-02-27 13:49:27 -0300

    feat: Change background for video player

--- desafio1/js/script.js
+++ desafio1/js/script.js
@@ -13,30 +13,32 @@ document.addEventListener("DOMContentLoaded", function() {
 function criarMorcego() {
     console.log("btn clicado");
 
-    let videoContainer = document.getElementById("video-container");
-    let video = document.getElementById("video");
+    const somMorcego = new Audio("/audio/bat_fly.mp3");
+    somMorcego.play();
 
+    let videoContainer = document.getElementById("videoContainer");
+    let videoBtn = document.getElementById("videoBtn");
     videoContainer.style.display = "block";
-    video.src ="https://www.youtube.com/embed/ddyKLEb_G38?autoplay=1&mute=1&loop=1&playlist=ddyKLEb_G38";
-    video.autoplay = true;
-    const botao = document.getElementById("btnCriar");
-    botao.style.display = "none";
+    videoBtn.play();
+
+    //const botao = document.getElementById("btnCriar");
+    //botao.style.display = "none";
     
     
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 20; i++) {
         const morcego = document.createElement("img");
         morcego.src = "/img/morcego_unic.png";
         morcego.style.position = "absolute";
         morcego.style.width = "300px";
         morcego.style.left = Math.random() * window.innerWidth + "px";
         morcego.style.top = Math.random() * window.innerHeight + "px";
-        morcego.style.transition = "transform 2s linear";
+        morcego.style.transition = "transform 5.5s linear";
 
         document.body.appendChild(morcego);
 
         setTimeout(() => {
             morcego.style.transform = "translateY(-100vh) rotateY(45deg) rotateX(45deg) scale(1.5)";
-            setTimeout(() => morcego.remove(), 2000);
-        }, 100);
+            setTimeout(() => morcego.remove(), 5000);
+        }, 200);
     }
 }
