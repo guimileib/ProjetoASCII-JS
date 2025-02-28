9b1b79c1aab690403d0c1b7420dcb0af8d08151a
guimileib <guilhermemileib@gmail.com>
2025-02-28 19:49:27 -0300

    feat:I implemented the dark/light mode theme on the website

--- projeto_final/js/script.js
+++ projeto_final/js/script.js
@@ -15,3 +15,25 @@ function mudarSlide(direcao) {
     let deslocamento = -slideIndex * 100;
     slider.style.transform = `translateX(${deslocamento}%)`;
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const darkModeToggle = document.getElementById("darkModeToggle");
+
+    darkModeToggle.addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    });
+
+    darkModeToggle.addEventListener("click", function () {
+        document.body.classList.toggle("dark-mode");
+
+        if (document.body.classList.contains("dark-mode")) {
+            localStorage.setItem("theme", "dark");
+        } else {
+            localStorage.setItem("theme", "light");
+        }
+    });
+
+    if (localStorage.getItem("theme") === "dark") {
+        document.body.classList.add("dark-mode");
+    }
+});
