4a1ac730f3fba1acbd952e62b8cfc082bbb5f67a
guimileib <guilhermemileib@gmail.com>
2025-02-28 15:05:58 -0300

    feat: Completed task for challenge 1

--- desafio1/js/script.js
+++ desafio1/js/script.js
@@ -6,7 +6,6 @@ document.addEventListener("DOMContentLoaded", function() {
     let botao = document.getElementById("btnCriar");
 
     videoContainer.style.display = "block";
-    videoInicial.play();
 
     document.body.addEventListener("click", function iniciarVideo() {
         videoInicial.play().catch(error => console.error("Erro ao tentar iniciar o vídeo:", error));
@@ -26,9 +25,6 @@ document.addEventListener("DOMContentLoaded", function() {
 function criarMorcego() {
     console.log("btn clicado");
 
-    const somMorcego = new Audio("/audio/bat_fly.mp3");
-    somMorcego.play();
-
     let videoInicial = document.getElementById("videoInicial");
     let videoContainer = document.getElementById("videoContainer");
     let videoBtn = document.getElementById("videoBtt");
@@ -39,27 +35,43 @@ function criarMorcego() {
     videoBtn.style.display = "block";
     videoBtn.style.visibility = "visible";
     videoBtn.style.opacity = "1";
-    videoBtn.play();
-
+    videoBtn.play()
+    
     botao.style.display = "none";
 
+    setTimeout(() => {
+        let somMorcego = new Audio("/desafio1/audio/bat_fly.mp3");
+        somMorcego.play();
+    }, 1000);
+
     for (let i = 0; i < 20; i++) {
+
         setTimeout(() => {
-            const morcego = document.createElement("img");
-            morcego.src = "/desafio1/img/morcego_unic.png";
-            morcego.style.position = "absolute";
-            morcego.style.width = "300px";
-            morcego.style.left = Math.random() * window.innerWidth + "px";
-            morcego.style.top = Math.random() * window.innerHeight + "px";
-            morcego.style.transition = "transform 5.5s linear";
-    
-            document.body.appendChild(morcego);
-    
             setTimeout(() => {
-                morcego.style.transform = "translateY(-100vh) rotateY(45deg) rotateX(45deg) scale(1.5)";
-                setTimeout(() => morcego.remove(), 5000);
-            }, 200);
-        }, i * 500); 
+                const morcego = document.createElement("img");
+                morcego.src = "/desafio1/img/morcego_unic.png";
+                morcego.style.position = "absolute";
+                morcego.style.width = "300px";
+                morcego.style.left = Math.random() * window.innerWidth + "px";
+                morcego.style.top = Math.random() * window.innerHeight + "px";
+                morcego.style.transition = "transform 5.5s linear";
+        
+                document.body.appendChild(morcego);
+        
+                setTimeout(() => {
+                    morcego.style.transform = "translateY(-100vh) rotateY(45deg) rotateX(45deg) scale(1.5)";
+                    setTimeout(() => morcego.remove(), 5000);
+                }, 200);
+            }, 1000);
+        
+        }, i * 500);
     }   
+
+    videoBtn.addEventListener("ended", function() {
+        console.log("Video finalizado, botao aparecendo");
+        videoBtn.style.display = "none";
+        videoContainer.style.background = "black";
+    });
+
     
 }
